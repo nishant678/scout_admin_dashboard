a4e0597e69a8e48b88927205e0a47604eac89804
nishantGurutu <this.is.nishantkumar1@gmail.com>
2026-06-15 00:29:04 +0530

    nbmvhgj

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE = 'http://localhost:8080/api';
-// const API_BASE = import.meta.env.VITE_API_URL || 'https://scout-spring-backend.onrender.com/api';
+// const API_BASE = 'http://localhost:8080/api';
+const API_BASE = 'https://scout-spring-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
